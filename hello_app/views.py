2dfec36ef0682e40ec1d2e4eb570707ec3153e2f
Gilbert (Your Majesty, My Lord) <gilbert.jacque@microsoft.com>
2023-02-11 18:15:19 +0100

    import cancelled all

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,6 +1,6 @@
-import requests, os, uuid, json
-from dotenv import load_dotenv
-load_dotenv()
+# import requests, os, uuid, json
+# from dotenv import load_dotenv
+# load_dotenv()
 
 from datetime import datetime
 from flask import Flask, render_template
