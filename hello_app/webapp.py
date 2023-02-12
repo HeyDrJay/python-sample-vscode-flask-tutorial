ad127ed53a083da1fc9f8eb4d77ba2c4c573ebce
Gilbert (Your Majesty, My Lord) <gilbert.jacque@microsoft.com>
2023-02-12 18:24:33 +0100

    import

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -1,14 +1,14 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command. 
 from . import views  # For import side-effects of setting up routes. 
-import requests, os, uuid, json
+# import requests, os, uuid, json
 from dotenv import load_dotenv
 load_dotenv()
 
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
 # print('http://127.0.0.1:5000/hello/VSCode')
 ###################################################
-# strating here, we define the translation service#
+# starting here, we define the translation service#
 ###################################################
 @app.route('/', methods=['POST'])
 def index_post():
