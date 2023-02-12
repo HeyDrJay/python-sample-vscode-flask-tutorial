483e987e5efb6779fe2f6c5d058abe378fccc0a4
Gilbert (Your Majesty, My Lord) <gilbert.jacque@microsoft.com>
2023-02-12 18:59:33 +0100

    alle imports wieder raus, req.txt noch drin

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -1,13 +1,10 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command. 
 from . import views  # For import side-effects of setting up routes. 
-import Requests, os, uuid, json
-from dotenv import load_dotenv
-load_dotenv()
-
-# import requests, os, uuid, json
+# import Requests, os, uuid, json
 # from dotenv import load_dotenv
 # load_dotenv()
+
 
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
 # print('http://127.0.0.1:5000/hello/VSCode')
