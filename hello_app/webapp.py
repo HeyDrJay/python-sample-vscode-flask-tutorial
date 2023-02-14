81be5f61314f7d0c9d3a7747b303ec93a1c093b7
Gilbert (Your Majesty, My Lord) <gilbert.jacque@microsoft.com>
2023-02-14 18:36:58 +0100

    only import

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -2,8 +2,8 @@
 from . import app    # For application discovery by the 'flask' command. 
 from . import views  # For import side-effects of setting up routes. 
 import requests, os, uuid, json
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
