ca25fda86645a1e6c0690e9ef849e98d26664c87
Gilbert (Your Majesty, My Lord) <gilbert.jacque@microsoft.com>
2023-02-14 18:23:24 +0100

    requests per pip installiert

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -1,9 +1,9 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command. 
 from . import views  # For import side-effects of setting up routes. 
-# import Requests, os, uuid, json
-# from dotenv import load_dotenv
-# load_dotenv()
+import requests, os, uuid, json
+from dotenv import load_dotenv
+load_dotenv()
 
 
 # Time-saver: output a URL to the VS Code terminal so you can easily Ctrl+click to open a browser
