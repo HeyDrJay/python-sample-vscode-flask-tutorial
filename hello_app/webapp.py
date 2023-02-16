a05c80f5d6106047e0e36317cd8a9f14d63b07e5
Gilbert (Your Majesty, My Lord) <gilbert.jacque@microsoft.com>
2023-02-16 17:16:42 +0100

    routes redefined

--- hello_app/webapp.py
+++ hello_app/webapp.py
@@ -1,6 +1,6 @@
 # Entry point for the application.
 from . import app    # For application discovery by the 'flask' command. 
-# from . import views  # For import side-effects of setting up routes. 
+from . import views  # For import side-effects of setting up routes. 
 import requests, os, uuid, json
 from dotenv import load_dotenv
 load_dotenv()
